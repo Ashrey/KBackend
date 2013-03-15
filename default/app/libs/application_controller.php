3dabd8e640fa1e9fe04e9d4bb635d720bde801ea
Alberto Berroteran <aberroteran@live.com>
2013-03-15 13:19:29 -0430

    Agregado el nuevo core y los archivos básicos de backend

--- default/app/libs/application_controller.php
+++ default/app/libs/application_controller.php
@@ -1,48 +1,31 @@
 <?php
 /**
- * @category Kumbia
- * @package ControllerDeprecated
- * @deprecated Ahora se usa AppController.
- * Se eliminará despues de la beta2
- *
- * Antiguo ApplicationController desaconsejado, ahora se usa el AppController.
- *
- * Todos los controladores heredan de esta clase en un nivel superior
- * por lo tanto los métodos aquí definidos estan disponibles para
+ * Antiguo Application Controller desaconsejado, ahora se usa el AppController
+ * Todas las controladores heredan de esta clase en un nivel superior
+ * por lo tanto los metodos aqui definidos estan disponibles para
  * cualquier controlador.
- */
-/**
- * @see Tags
- */
+ *
+ * @category Kumbia
+ * @package Controller
+ * @deprecated
+ **/
+
+// Para cargar los helpers antiguos
 require_once CORE_PATH . 'extensions/helpers/tags.php';
 
-/**
- * @see ControllerDeprecated Antiguo controlador por compatibilidad
- */
+// @see Controller antiguo por compatibilidad
 require_once CORE_PATH . 'kumbia/controller_deprecated.php';
 
-/**
- * (Obsoleto) Clase controladora que extienden los demás controllers 
- *
- * @deprecated Ahora se usa AppController.
- * Se eliminará despues de la beta2.
- * Se mantiene para portar apps fácilmente de 0.5 y beta1.
- *
- * @category Kumbia
- * @package ControllerDeprecated
- */
-class ApplicationController extends ControllerDeprecated
-{
+class ApplicationController extends ControllerDeprecated {
 
-    final protected function initialize()
-    {
+	final protected function initialize()
+	{
+	}
 
-    }
+	final protected function finalize()
+	{
+		parent::finalize();// No tocar
+		// Añadir código aqui
 
-    final protected function finalize()
-    {
-        parent::finalize(); // No tocar
-        // Añadir código aqui
-    }
-
+	}
 }
--- default/app/libs/scaffold_controller.php
+++ default/app/libs/scaffold_controller.php
@@ -1,48 +1,95 @@
 <?php
 
-/**
- * Controlador base para la construcción de CRUD para modelos rapidamente
- *
- * @category Kumbia
- * @package Controller
- */
-class ScaffoldController extends AdminController
-{
+class ScaffoldController extends AdminController {
 
-    public $scaffold = 'kumbia';
+    public $scaffold = 'bootstrap';
     public $model;
+    
+    protected $show_cols = array();
+    
+    protected function after_filter() {
+        if (Input::isAjax()) {
+            View::select('ajax', null);
+        }
+    }
 
-    public function index($page=1)
-    {
-        $this->results = Load::model($this->model)->paginate("page: $page", 'order: id desc');
+    protected function before_filter() {
+        if ($this->scaffold == 'static')
+            View::template(null);
+    }
+
+    public function index($page = 1) {
+        /* Se asegura de que siempre exista el filtro 
+         * en el espacio de nombres */
+        if (!Session::has('filter', $this->model)) {
+            Session::set('filter', array(), $this->model);
+        }
+
+        /* Analiza las peticiones */
+        if (Input::post('clear')) {/* Elimina todos los filtros */
+            Session::set('filter', array(), $this->model);
+        } elseif (Input::post('add')) {/* Agrega un filtro */
+            $cond = Session::get('filter', $this->model);
+            $nuevo = Input::post('filter');
+            $nuevo['val'] = empty($nuevo['val']) && $nuevo['val'] !== '0' ?
+                    'NULL' : '"' . addslashes($nuevo['val']) . '"';
+            $cond[uniqid()] = $nuevo;
+            Session::set('filter', $cond, $this->model);
+        } elseif (Input::post('remove')) {/* Remueve un filtro */
+            $cond = Session::get('filter', $this->model);
+            $key = Input::post('remove');
+            unset($cond[$key]);
+            Session::set('filter', $cond, $this->model);
+        }
+        /* Hace Redireccion en caso de peticones POST */
+       if ($_SERVER['REQUEST_METHOD'] == 'POST')
+            Router::redirect();
+        
+        
+        $cond = Session::get('filter', $this->model);
+        /* Crea los filtros */
+        $filter = array('1 = 1');
+        foreach ($cond as $val) {
+            $filter[] = implode(' ', $val);
+        }
+        $cond = implode(' AND ', $filter);
+        $cols = !empty($this->show_cols) || is_array($this->show_cols)?
+            implode(',', $this->show_cols) :'*';
+        $this->cols = $this->show_cols;
+        $this->results = Load::model($this->model)->paginate($cond,
+                "page: $page", "columns: $cols");
     }
 
     /**
      * Crea un Registro
      */
-    public function crear()
-    {
-        if (Input::hasPost($this->model)) {
-
-            $obj = Load::model($this->model);
-            //En caso que falle la operación de guardar
-            if (!$obj->save(Input::post($this->model))) {
-                Flash::error('Falló Operación');
-                //se hacen persistente los datos en el formulario
-                $this->{$this->model} = $obj;
-                return;
+    public function crear() {
+        try {
+            if (Input::hasPost($this->model)) {
+                $obj = Load::model($this->model);
+                //En caso que falle la operación de guardar
+                if (!$obj->save(Input::post($this->model))) {
+                    Flash::error('Falló Operación');
+                    //se hacen persistente los datos en el formulario
+                    $this->{$this->model} = $obj;
+                    return;
+                }else{
+                    Flash::success('Agregegado correctamente');
+                    Router::redirect();
+                }
             }
-            return Redirect::to();
+            // Solo es necesario para el autoForm
+            $this->{$this->model} = Load::model($this->model);
+        } catch (KumbiaException $e) {
+            $this->{$this->model} = Load::model($this->model);
+            Flash::error($e);
         }
-        // Solo es necesario para el autoForm
-        $this->{$this->model} = Load::model($this->model);
     }
 
     /**
      * Edita un Registro
      */
-    public function editar($id)
-    {
+    public function editar($id) {
         View::select('crear');
 
         //se verifica si se ha enviado via POST los datos
@@ -53,7 +100,7 @@ class ScaffoldController extends AdminController
                 //se hacen persistente los datos en el formulario
                 $this->{$this->model} = Input::post($this->model);
             } else {
-                return Redirect::to();
+                return Router::toAction('');
             }
         }
 
@@ -64,21 +111,26 @@ class ScaffoldController extends AdminController
     /**
      * Borra un Registro
      */
-    public function borrar($id)
-    {
+    public function borrar($id) {
         if (!Load::model($this->model)->delete((int) $id)) {
             Flash::error('Falló Operación');
+        }else{
+            Flash::ok('Borrado correctamente');
         }
         //enrutando al index para listar los articulos
-        Redirect::to();
+        Router::redirect();
     }
 
     /**
      * Ver un Registro
      */
-    public function ver($id)
-    {
+    public function ver($id) {
         $this->result = Load::model($this->model)->find_first((int) $id);
+    }
+    
+    public function update($field){
+        Scaffold::update(Load::model($this->model), $field);
+        die();
     }
 
 }
