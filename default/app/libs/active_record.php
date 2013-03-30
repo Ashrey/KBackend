0fa136442ecfa402b5ad187ce99cc7f4b54bce35
Alberto Berroteran <aberroteran@live.com>
2013-03-30 13:49:53 -0430

    Las auditorias no depende del active record

--- default/app/libs/active_record.php
+++ default/app/libs/active_record.php
@@ -14,47 +14,7 @@
 Load::coreLib('kumbia_active_record');
 
 class ActiveRecord extends KumbiaActiveRecord implements ArrayAccess {
-    /**
-     * Para llevar auditorias.
-     * 
-     * Es bueno que si se reescribe el metodo, se llame al parent, para 
-     * que no se deje de ejecutar el log.
-     * 
-     */
-    protected function after_save() {
-        $this->log();
-    }
-
-    /**
-     * Para llevar auditorias.
-     * 
-     * Es bueno que si se reescribe el metodo, se llame al parent, para 
-     * que no se deje de ejecutar el log.
-     */
-    protected function after_delete() {
-        $this->log();
-    }
-
-    /**
-     * LLeva un control de auditorias para los cambios que se hagan en la BD.
-     * 
-     * Registra las consultas que alteren la data de la bd.
-     * 
-     * Actualmente no funciona con los INSERT INTO.
-     * 
-     */
-    protected function log() {
-        if ($this->source != 'auditorias') { //mucho ojo con esto
-            //solo debemos hacer el log si la tabla no es la de auditorias;
-            $tabla = $this->schema ? "$this->schema.$this->source" : $this->source;
-            $sql = trim($this->db->last_sql_query());
-            $tmp = explode(' ', $sql);
-            $type = isset($tmp[0]) && is_string($tmp) ?$tmp[0] :'UNKNOW';
-            if ($type != 'SELECT'){
-                Acciones::add($type, $tabla, $sql);
-            }
-        }
-    }
+    protected $logger  = true;
 
     public function offsetSet($indice, $valor) {
         if (!is_null($indice)) {
@@ -73,5 +33,4 @@ class ActiveRecord extends KumbiaActiveRecord implements ArrayAccess {
     public function offsetGet($indice) {
         return $this->offsetExists($indice) ? $this->{$indice} : NULL;
     }
-
 }
