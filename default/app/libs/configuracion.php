59d983816cc47ca0e94bfee1bf2a455a0768cc63
Alberto Berroteran <aberroteran@live.com>
2013-03-31 20:00:15 -0430

    Guardado de configuraciones

--- default/app/libs/configuracion.php
+++ default/app/libs/configuracion.php
@@ -1,26 +1,27 @@
 <?php
+
 /**
-* Backend - KumbiaPHP Backend
-* PHP version 5
-* LICENSE
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-* @package Libs
-* @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
-* @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
-*/
+ * Backend - KumbiaPHP Backend
+ * PHP version 5
+ * LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Libs
+ * @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
+ * @author Manuel José Aguirre Garcia <programador.manuel@gmail.com>
+ */
 class Configuracion {
 
     protected static $_archivo_ini = NULL;
@@ -29,9 +30,9 @@ class Configuracion {
     public static function leer($file) {
         self::$_archivo_ini = APP_PATH . "config/{$file}.ini";
         self::$_configuracion = parse_ini_file(self::$_archivo_ini, true);
-        foreach(self::$_configuracion as $key => $section){
+        foreach (self::$_configuracion as $key => $section) {
             foreach ($section as $variable => $valor) {
-                 if ($valor == 1) {
+                if ($valor == 1) {
                     self::$_configuracion[$key][$variable] = 'On';
                 } elseif (empty($valor)) {
                     self::$_configuracion[$key][$variable] = 'Off';
@@ -46,22 +47,20 @@ class Configuracion {
     }
 
     public static function guardar() {
-        $html = <<<TEXTO
-;; Configuracion de Aplicacion
-
-TEXTO;
-        foreach(self::$_configuracion as $key => $section){
+        $html = '';
+        foreach (self::$_configuracion as $key => $section) {
             $html .= "[$key]" . PHP_EOL;
             foreach ($section as $variable => $valor) {
-                if ( in_array($valor , array('On', 'Off')) || is_numeric($valor) ){
-                    $html .= "$variable = $valor" . PHP_EOL;                    
-                }else{
+                $valor = empty($valor) ? ' ':$valor;
+                if (in_array($valor, array('On', 'Off')) || is_numeric($valor)) {
+                    $html .= "$variable = $valor" . PHP_EOL;
+                } else {
                     $valor = h($valor);
-                    $html .= "$variable = \"$valor\"" . PHP_EOL;                    
+                    $html .= "$variable = \"$valor\"" . PHP_EOL;
                 }
-                
             }
         }
         return file_put_contents(self::$_archivo_ini, $html);
     }
+
 }
