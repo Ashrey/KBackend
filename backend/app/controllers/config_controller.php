22b44b83109a1fae3562ae453fbb789fb01d8935
Alberto Berroteran <aberroteran@live.com>
2013-05-23 15:43:50 -0430

    Cambios en la licencia

--- backend/app/controllers/config_controller.php
+++ backend/app/controllers/config_controller.php
@@ -1,34 +1,19 @@
 <?php
 /**
-* Backend - KumbiaPHP Backend
-* PHP version 5
-* LICENSE
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-* @package Controller
-* @license http://www.gnu.org/licenses/agpl.txt GNU AFFERO GENERAL PUBLIC LICENSE version 3.
-* @author Alberto Berroteran <programador.manuel@gmail.com>
-*/
-class ConfigController extends AdminController {
+ * KBackend
+ * PHP version 5
+ * @package Libs
+ * @license https://raw.github.com/Ashrey/KBackend/master/LICENSE.txt
+ * @author KumbiaPHP Development Team
+ */
+class ConfigController extends \KBackend\Libs\AuthController {
 
 	
     public function index() {
         try {
             $file = 'backend';
             $this->config = $file;
-            $temp = Configuracion::leer($file);
+            $temp = \KBackend\Libs\ConfigINI::read($file);
             $this->values = $temp;
             if (Input::hasPost('config')) {
                 $config = Input::post('config');
