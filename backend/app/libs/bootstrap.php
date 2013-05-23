a9b9aa6015659f57f8b737041505ecc50fb41c91
Alberto Berroteran <aberroteran@live.com>
2013-05-23 15:48:43 -0430

    Agregada autocarga de archivos del backend usando namespace

--- backend/app/libs/bootstrap.php
+++ backend/app/libs/bootstrap.php
@@ -1,22 +1,17 @@
 <?php
 // Bootstrap de la aplicacion para personalizarlo
 // Para cargar cambia en public/index.php el require del bootstrap a app
-
-require_once CORE_PATH . 'kumbia/config.php';
-
-// Lee la configuracion
-$config = Config::read('config');
-
-error_reporting(E_ALL ^ E_STRICT);
-ini_set('display_errors', 'On');
-
-if ($config['application']['production']) {
-    if (!$config['application']['debug']) {
-        error_reporting(0);
-        ini_set('display_errors', 'On');
-    }
-}
-
-// Arranca KumbiaPHP
+function  autoload($name){
+	if(strncmp($name, 'KBackend', 7) == 0){
+		$split = explode('\\', $name);
+		$class = $split[2];
+		if ($split[1] == 'Model'){
+			include APP_PATH . "models/$class.php";
+        }elseif($split[1] == 'Libs'){
+			include APP_PATH . "libs/$class.php";
+		}
+	}
+} 
+spl_autoload_register ('autoload');
 require_once CORE_PATH . 'kumbia/bootstrap.php';
 
