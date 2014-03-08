bc7f5fbc208a6a74296ea728c4f130efaec98168
Alberto Berroteran <aberroteran@live.com>
2014-03-08 01:01:14 -0430

    Cambió el sitio del logout

--- backend/app/controllers/user_controller.php
+++ backend/app/controllers/user_controller.php
@@ -56,4 +56,12 @@ class UserController extends \KBackend\Libs\ScaffoldController{
         }
         return Redirect::toAction('');
     }
+
+    /**
+     * hace el logout
+     */
+    public function logout() {
+        parent::logout();
+    }
+    
 }
