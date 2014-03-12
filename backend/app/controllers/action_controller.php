5eb77c575e412a4b4fab40fe03286c02943357bd
Alberto Berroteran <aberroteran@live.com>
2014-03-12 15:02:01 -0430

    Adapter for New ActiveRecord

--- backend/app/controllers/action_controller.php
+++ backend/app/controllers/action_controller.php
@@ -6,11 +6,15 @@
  * @license https://raw.github.com/Ashrey/KBackend/master/LICENSE.txt
  * @author KumbiaPHP Development Team
  */
+
+use \KBackend\Model\User;
+use \KBackend\Model\Action;
+
 class ActionController extends  \KBackend\Libs\AuthController{
 
     public function index($pag= 1) {
         try {
-            $this->user = \KBackend\Model\User::_numAcciones($pag);
+            $this->user = User::actions($pag);
         } catch (KumbiaException $e) {
             View::excepcion($e);
         }
@@ -20,11 +24,9 @@ class ActionController extends  \KBackend\Libs\AuthController{
         $this->url = "action/user/$id";
         try {
             $id = (int)$id;
-            $usr = new \KBackend\Model\User();
-            $aud = new \KBackend\Model\Action();
-            $this->usuario = $usr->find_first($id);
-            $this->result = $aud->byUser($id, $pagina);
-            if (!$this->result->items) {
+            $this->usuario = User::get($id);
+            $this->result =  Action::byUser($id, $pagina);
+            if (!$this->result) {
                 \Flash::info('Este usuario no ha realizado ninguna acción en el sistema');
                 return Redirect::to();
             }
