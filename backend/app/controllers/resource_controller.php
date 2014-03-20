03552b4527d8d52c6dc3c9f42fe35c68adddf400
Alberto Berroteran <aberroteran@live.com>
2014-03-19 21:32:21 -0430

    Remove un used code

--- backend/app/controllers/resource_controller.php
+++ backend/app/controllers/resource_controller.php
@@ -11,22 +11,4 @@ class ResourceController extends \KBackend\Libs\ScaffoldController{
 	protected $_model = '\KBackend\Model\Resource';
 	
 	protected $_title = 'Resource';
-
-    public function escaner($pagina = 1) {
-        try {
-            $recurso = new Recursos();
-            if (Input::hasPost('descripcion') && Input::hasPost('check')) {
-                //para obtener los valores de los nuevos controladores
-                $recurso->obtener_recursos_nuevos($pagina);
-                if ($recurso->guardar_nuevos()) {
-                    Flash::valid('Recursos Guardados Exitosamente');
-                } else {
-                    Flash::warning('Complete los datos requeridos e intente nuevamente');
-                }
-            }
-            $this->recursos = $recurso->obtener_recursos_nuevos($pagina);
-        } catch (KumbiaException $e) {
-            View::excepcion($e);
-        }
-    }
 }
--- backend/app/models/Resource.php
+++ backend/app/models/Resource.php
@@ -8,59 +8,5 @@ namespace KBackend\Model;
  * @author KumbiaPHP Development Team
  */
 class Resource extends \KBackend\Libs\ARecord {
-	protected $source = '_resource';
 
-    protected function initialize() {
-   }
-
-    /**
-     * Obtiene los recursos que no se han agregado al al bd.
-     * 
-     * @param  integer $pagina 
-     * @return array          
-     */
-    public function obtener_recursos_nuevos($pagina = 1) {
-        $recursos = LectorRecursos::obtenerRecursos();
-        foreach ($recursos as $index => $re) {
-            if ($this->exists('recurso = \'' . $re['recurso'] . '\'')) {
-                unset($recursos[$index]);
-            }
-        }
-        $recursos = LectorRecursos::paginar($recursos, $pagina, Config::get('backend.app.per_page'));
-        $this->recursos_nuevos = $recursos->items;
-        array_unshift($this->recursos_nuevos, null);
-        return $recursos;
-    }
-
-    /**
-     * Guarda los recursos que aun no estan en bd y fueron seleccionados
-     * por el usuario.
-     * 
-     * @return boolean 
-     */
-    public function guardar_nuevos() {
-        $chequeados = Input::post('check');
-        $descripciones = Input::post('descripcion');
-        $activos = Input::post('activo');
-        $this->begin();
-        if (!$chequeados) {
-            return FALSE;
-        }
-        foreach ($chequeados as $valor) {
-            if (empty($descripciones[$valor])) {
-                Flash::error('Hay recursos seleccionados que no tienen descripción');
-                $this->rollback();
-                return FALSE;
-            }
-            $data = $this->recursos_nuevos[$valor];
-            $data['descripcion'] = $descripciones[$valor];
-            $data['activo'] = $activos[$valor];
-            if (!$this->create($data)) {
-                $this->rollback();
-                return FALSE;
-            }
-        }
-        $this->commit();
-        return TRUE;
-    }
 }
