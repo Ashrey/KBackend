dacd3f293ac93e29c4a1de50bb13e577acd0788f
Alberto Berroteran <aberroteran@live.com>
2014-03-23 00:12:41 -0430

    New View

---
+++ backend/app/libs/Template.php
@@ -0,0 +1,105 @@
+<?php
+namespace KBackend\Libs;
+/**
+ * KBackend
+ * PHP version 5
+ * @package Libs
+ * @license https://raw.github.com/Ashrey/KBackend/master/LICENSE.txt
+ * @author KumbiaPHP Development Team
+ */
+require_once CORE_PATH . 'kumbia/kumbia_view.php';
+use \Haanga;
+class Template extends \KumbiaView {
+    /**
+     * Muestra las excepciones generadas y crea un log de las mismas.
+     * @param  Exception $e [description]
+     */
+    public static function excepcion(\Exception $e) {
+        Flash::warning('Ha Ocurrido un error');
+        if (Config::get('config.application.log_exception') || !PRODUCTION) {
+            Flash::error($e->getMessage());
+        }else{
+            Flash::error($e->getTraceAsString());
+        }
+        Logger::log("Exception $e");
+        Flash::info('Si el problema persiste por favor informe al administrador del sistema');
+    }
+
+    /**
+     * Muestra la vista de notFound de la app.
+     * 
+     */
+    public function notFound() {
+        throw new KumbiaException(NULL, 'no_controller');
+    }
+
+    /**
+     * Renderiza la vista
+     * @overload
+     * @todo add the cache
+     * @param Controller $controller
+     */
+    public static function render($controller)
+    {
+        /*Establece las configuraciones de haanga*/
+        Haanga::configure(array(
+            'template_dir' => APP_PATH.'views',
+            'cache_dir' => APP_PATH.'temp/cache/haanga',
+            'compiler' => array( /* opts for the tpl compiler */
+                'strip_whitespace' => TRUE,
+                'allow_exec'  => TRUE
+            ),
+        ));
+
+
+        /*Si no hay nada termina el proceso y descarga el buffer*/
+        if (!self::$_view && !self::$_template)
+            return ob_end_flush();
+         
+        // Guarda el controlador
+        self::$_controller = $controller;
+         
+        /*extrae las variables del controllador*/
+        $vars = get_object_vars($controller);
+        
+         // carga la vista si no esta en produccion o se usa scaffold o no hay contenido cargado
+        if (!PRODUCTION || $scaffold || !self::$_content) {
+            // Carga el contenido del buffer de salida
+            self::$_content = ob_get_clean();
+            $file = static::getFile();
+            self::$_content = static::getTpl($file, $vars);
+        } else {
+            ob_clean();
+        }
+        echo self::$_content;
+    }
+
+    static protected function getFile(){
+        /*Captura el posible scaffold*/
+        $c = self::$_controller;
+        $scaffold = $c->scaffold;
+        $file =  self::getPath();
+        if (!is_file(KBACKEND_PATH ."/views/$file")){
+            $view = self::$_view;
+            return ( $scaffold && $view ) ?
+                "_shared/scaffolds/$scaffold/$view.phtml":
+                self::$_template. '.phtml';
+        }
+        return $file;
+    }
+
+    public static function getTpl($file, $vars){
+        return Haanga::Load($file, $vars, true);
+    }
+
+
+    public static function content()
+    {
+        if (isset($_SESSION['KUMBIA.CONTENT'])) {
+            $content = $_SESSION['KUMBIA.CONTENT'];
+            unset($_SESSION['KUMBIA.CONTENT']);
+            return $content;
+        }
+        return self::$_content;
+    }
+}
--- backend/app/libs/view.php
+++ backend/app/libs/view.php
@@ -1,103 +1,12 @@
 <?php
-
 /**
- * Esta clase permite extender o modificar la clase ViewBase de Kumbiaphp.
- *
- * @category KumbiaPHP
- * @package View
- * */
-// @see KumbiaView
-require_once CORE_PATH . 'kumbia/kumbia_view.php';
-use \KBackend\Libs\Logger;
+ * KBackend
+ * PHP version 5
+ * @package Libs
+ * @license https://raw.github.com/Ashrey/KBackend/master/LICENSE.txt
+ * @author KumbiaPHP Development Team
+ */
 
-class View extends KumbiaView {
-
-    /**
-     * Muestra las excepciones generadas y crea un log de las mismas.
-     * @param  Exception $e [description]
-     */
-    public static function excepcion(\Exception $e) {
-        Flash::warning('Ha Ocurrido un error');
-        if (Config::get('config.application.log_exception') || !PRODUCTION) {
-            Flash::error($e->getMessage());
-        }
-        if (!PRODUCTION) {
-            Flash::error($e->getTraceAsString());
-        }
-        Logger::log("Exception $e");
-        Flash::info('Si el problema persiste por favor informe al administrador del sistema');
-    }
-
-    /**
-     * Muestra la vista de notFound de la app.
-     * 
-     */
-    public function notFound() {
-        throw new KumbiaException(NULL, 'no_controller');
-    }
-
-    /**
-     * Renderiza la vista
-     * @overload
-     * @todo add the cache
-     * @param Controller $controller
-     */
-    public static function render($controller)
-    {
-        /*Establece las configuraciones de haanga*/
-        Haanga::configure(array(
-            'template_dir' => APP_PATH.'views',
-            'cache_dir' => APP_PATH.'temp/cache/haanga',
-            'compiler' => array( /* opts for the tpl compiler */
-                'strip_whitespace' => TRUE,
-                'allow_exec'  => TRUE
-            ),
-        ));
-
-
-        /*Si no hay nada termina el proceso y descarga el buffer*/
-        if (!self::$_view && !self::$_template)
-            return ob_end_flush();
-         
-        // Guarda el controlador
-        self::$_controller = $controller;
-        /*Captura el posible scaffold*/
-        $scaffold = $controller->scaffold;
-        
-        /*extrae las variables del controllador*/
-        $vars = get_object_vars($controller);
-        
-         // carga la vista si no esta en produccion o se usa scaffold o no hay contenido cargado
-        if (!PRODUCTION || $scaffold || !self::$_content) {
-            // Carga el contenido del buffer de salida
-            self::$_content = ob_get_clean();
-            $file = self::getPath();
-            if (!is_file(APP_PATH ."views/$file")){
-                $view = self::$_view;
-                $file = ( $scaffold && $view ) ?
-                    "_shared/scaffolds/$scaffold/$view.phtml":
-                    self::$_template. '.phtml';
-            }
-            self::$_content = Haanga::Load($file, $vars, true);
-        } else {
-            ob_clean();
-        }
-        echo self::$_content;
-    }
-
-    public static function content()
-    {
-        if (isset($_SESSION['KUMBIA.CONTENT'])) {
-            $content = $_SESSION['KUMBIA.CONTENT'];
-            unset($_SESSION['KUMBIA.CONTENT']);
-            return $content;
-        }
-        return self::$_content;
-    }
-
-    static function aclGet($key){
-       return \KBackend\Libs\AuthACL::get($key);
-    }
-
-
+class View extends \KBackend\Libs\Template{
+    
 }
