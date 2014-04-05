ab3af157cacfabcac20e0eacd24508da02958652
Alberto Berroteran <aberroteran@live.com>
2014-04-05 18:45:02 -0430

    Optimize

--- backend/app/libs/ConfigINI.php
+++ backend/app/libs/ConfigINI.php
@@ -8,21 +8,16 @@ namespace KBackend\Libs;
  * @author KumbiaPHP Development Team
  */
 class ConfigINI{
-	/**
-	 * File to parse
-	 */
-	protected $_file = null;
-	
-	/**
-	 * Values and key of file
-	 */
-	protected $_value = array();
-	
-	/**
-	 * Last error ocurred
-	 */
-	protected $_lastError = 'unknoww error';
-	
+    /**
+     * File to parse
+     */
+    protected $_file = null;
+    
+    /**
+     * Values and key of file
+     */
+    protected $_value = array();
+    
     public function __construct ($file) {
         $this->_file = APP_PATH . "config/{$file}.ini";
         $_conf = parse_ini_file($this->_file, true);
@@ -38,46 +33,54 @@ class ConfigINI{
         $this->_value = $_conf;
     }
 
-    public function save($config) {
-		//assign new values
-		foreach ($config as $key => $value) {
-			$val = explode('.', $key);
+    /**
+     * Save the file
+     * @param Array $config
+     * @return bool
+     */
+    public function save(Array $config) {
+        $this->assign($config);
+        $str = $this->generate();
+        if(is_writable($this->_file)){
+            Logger::log("CONFIG archivo {$this->_file}.ini modificado");
+            return file_put_contents($this->_file, $str);
+        }
+        throw new \Exception('File not writable');
+    }
+    
+    /**
+     * Assign values
+     * @param Array $config values to assign
+     */
+    protected function assign(Array $config){
+        foreach ($config as $key => $value) {
+            $val = explode('.', $key);
             $this->_value[$val[0]][$val[1]]=$value;
-        }    
-        $html = '';
-        foreach ($this->_value as $key => $section) {
-            $html .= "[$key]" . PHP_EOL;
+        }   
+    }
+
+    /**
+     * Generate ini content
+     * @return string 
+     */
+    protected function generate(){
+        $buffer = ''
+         foreach ($this->_value as $key => $section) {
+            $buffer .= "[$key]" . PHP_EOL;
             foreach ($section as $variable => $valor) {
-                $valor = empty($valor) ? ' ':$valor;
-                if (in_array($valor, array('On', 'Off')) || is_numeric($valor)) {
-                    $html .= "$variable = $valor" . PHP_EOL;
-                } else {
-                    $html .= "$variable = \"$valor\"" . PHP_EOL;
-                }
+                $buffer .= (in_array($valor, array('On', 'Off')) || is_numeric($valor)) ?
+                    "$variable = $valor" : "$variable = \"$valor\"";
+                $buffer .=  PHP_EOL
             }
         }
-		if(is_writable($this->_file)){
-			Logger::log("CONFIG archivo {$this->_file}.ini modificado");
-			return file_put_contents($this->_file, $html);
-		}else{
-			$this->_lastError = '';
-			return false;
-		}
-       
+        return $buffer;
     }
-	
-	/**
-	 * Get array with file value
-	 */
-	public function getAll(){
-		return $this->_value;
-	}
-	
-	/**
-	 * Return last error
-	 */
-	public function getError(){
-		return $this->_lastError;
-	}
-	
+
+    /**
+     * Get array with file value
+     */
+    public function getAll(){
+        return $this->_value;
+    }
+    
 }
