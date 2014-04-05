c1493c9abae10cd966d0d55f7bceb9709a4c95c5
Alberto Berroteran <aberroteran@live.com>
2014-04-05 18:27:45 -0430

    Remove unused method

--- backend/app/controllers/user_controller.php
+++ backend/app/controllers/user_controller.php
@@ -10,45 +10,4 @@ class UserController extends \KBackend\Libs\ScaffoldController{
 	
 	protected $_model = '\KBackend\Model\User';
 
-    /**
-     * Activa un usuario desde el backend
-     * @param  int $id id del usuario a activar
-     */
-    public function activar($id) {
-        try {
-            $id = (int) $id;
-            $usuario = new Usuarios();
-            if (!$usuario->find_first($id)) { //si no existe el usuario
-                Flash::warning("No existe ningun usuario con id '{$id}'");
-            } else if ($usuario->activar()) {
-                Flash::valid("La Cuenta {$usuario->login} ({$usuario->nombres}) fue activada");
-            } else {
-                Flash::warning('No se Pudo Activar la cuenta');
-            }
-        } catch (KumbiaException $e) {
-            View::excepcion($e);
-        }
-        return Redirect::toAction('');
-    }
-
-    /**
-     * Desactiva un usuario desde el backend
-     * @param  int $id id del usuario a desactivar
-     */
-    public function desactivar($id) {
-        try {
-            $id = (int) $id;
-            $usuario = new Usuarios();
-            if (!$usuario->find_first($id)) { //si no existe el usuario
-                Flash::warning("No existe ningun usuario con id '{$id}'");
-            } else if ($usuario->desactivar()) {
-                Flash::valid("La Cuenta {$usuario->login} ({$usuario->nombres}) fue desactivada");
-            } else {
-                Flash::warning('No se pudo desactivar la cuentaS');
-            }
-        } catch (KumbiaException $e) {
-            View::excepcion($e);
-        }
-        return Redirect::toAction('');
-    }
 }
