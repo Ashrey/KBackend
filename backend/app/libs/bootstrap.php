f02197b80cb163eeafd82110ae0b3823a7c3179e
Alberto Berroteran <aberroteran@live.com>
2014-08-17 16:39:54 -0430

    Clean bootstrap

--- backend/app/libs/bootstrap.php
+++ backend/app/libs/bootstrap.php
@@ -1,4 +1,22 @@
 <?php
+$prod = false;
+$cache = dirname(__DIR__).'/temp/cache';
+$url = empty($_SERVER['PATH_INFO']) ?  '/' : $_SERVER['PATH_INFO'];
+$query = empty($_SERVER['QUERY_STRING'])? '': '?' .urldecode($_SERVER['QUERY_STRING']);
+$exc = $url . $query;
+$vars = array(
+    '%:last-update%'     => time(),
+    '%:error_reporting%' => !$prod ? 'On': 'Off',
+    '%:display_error%'   => !$prod ? 'On': 'Off',
+    '%:app_path%'        => dirname(__DIR__).'/',
+    '%:core_path%'       => dirname(dirname(dirname(__DIR__))) . '/core/',
+    '%:public%'          => rtrim(($_SERVER['REQUEST_URI']), $exc).'/'
+);
+$file = file_get_contents(__DIR__.'/template.tpl');
+$str = str_replace(array_keys($vars), array_values($vars), $file);
+file_put_contents("$cache/vars.php", $str);
+include "$cache/vars.php";
+
 require APP_PATH . 'libs/autoload.php';
 require APP_PATH . 'libs/init.php';
 require CORE_PATH . 'kumbia/bootstrap.php';
---
+++ backend/app/libs/template.tpl
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is automated generator
+ * Last Update: %:last-update%
+ */
+define('START_TIME', microtime(true));
+error_reporting('%:error_reporting%');
+ini_set('display_errors', '%:display_error%'); 
+define('APP_PATH', '%:app_path%');
+define('CORE_PATH', '%:core_path%');
+define('PUBLIC_PATH', '%:public%');
