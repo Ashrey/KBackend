854ae0b297cf265ed227864e366b66cf2ae4b638
Alberto Berroteran <aberroteran@live.com>
2014-10-28 18:31:19 -0430

    new config editor TO-DO

--- backend/app/controllers/config_controller.php
+++ backend/app/controllers/config_controller.php
@@ -6,13 +6,24 @@
  * @license https://raw.github.com/Ashrey/KBackend/master/LICENSE.txt
  * @author KumbiaPHP Development Team
  */
+use KBackend\Libs\Config;
 class ConfigController extends \KBackend\Libs\AuthController {
     public function index() {
         try {
-            $file = 'backend';
-            $this->config = $file;
-            $ini = new \KBackend\Libs\ConfigINI($file);
-            $this->values = $ini->getAll();
+            $this->values = Config::get('backend');
+            if (Input::hasPost('config')) {
+                $this->flash($ini->save(Input::post('config')),
+                    'Configuración fue Actualizada', 
+                    'No se pudo guardar');
+           }  
+        } catch (\Exception $e) {
+            View::excepcion($e);
+        }
+    }
+
+    public function section($sec) {
+        try {
+            $this->values = Config::get("backend.$sec");
             if (Input::hasPost('config')) {
                 $this->flash($ini->save(Input::post('config')),
                     'Configuración fue Actualizada', 
