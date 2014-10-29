a32324e5bc32eccac9b303364ecf707e05a9e0fb
Alberto Berroteran <aberroteran@live.com>
2014-10-28 20:12:58 -0430

    Temporal *TO DO*

--- backend/app/controllers/config_controller.php
+++ backend/app/controllers/config_controller.php
@@ -9,23 +9,14 @@
 use KBackend\Libs\Config;
 class ConfigController extends \KBackend\Libs\AuthController {
     public function index() {
-        try {
-            $this->values = Config::get('backend');
-            if (Input::hasPost('config')) {
-                $this->flash($ini->save(Input::post('config')),
-                    'Configuración fue Actualizada', 
-                    'No se pudo guardar');
-           }  
-        } catch (\Exception $e) {
-            View::excepcion($e);
-        }
+        $this->values = Config::get('backend');
     }
 
     public function section($sec) {
         try {
             $this->values = Config::get("backend.$sec");
             if (Input::hasPost('config')) {
-                $this->flash($ini->save(Input::post('config')),
+                $this->flash(FALSE,
                     'Configuración fue Actualizada', 
                     'No se pudo guardar');
            }  
